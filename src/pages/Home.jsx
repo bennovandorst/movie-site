47f10d77ef3aa9f0f22a67622946cf1b52177233
bennovandorst <9024828@student.zadkine.nl>
2024-10-31 22:40:35 +0100

    Marco borsato commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import MovieList from "../components/MovieList";
+import axios from 'axios';
 
 const Home = () => {
     const [movies, setMovies] = useState([]);
     const [searchTerm, setSearchTerm] = useState("")
+    const [randomMovieId, setRandomMovieId] = useState(null);
     
     useEffect(() => {
         const fetchMovies = async () => {
@@ -16,13 +18,34 @@ const Home = () => {
         fetchMovies();
     }, [searchTerm]);
 
+    useEffect(() => {
+        const fetchRandomMovie = async () => {
+            try {
+                const response = await axios.get('https://api.themoviedb.org/3/movie/popular', {
+                    params: {
+                        api_key: 'c7cf1f564fa32aed665c2abb44d2ffb9',
+                        language: 'en-US',
+                        page: 1
+                    }
+                });
+                const movies = response.data.results;
+                const randomIndex = Math.floor(Math.random() * movies.length);
+                setRandomMovieId(movies[randomIndex].original_title);
+            } catch (error) {
+                console.error('Error fetching random movie:', error);
+            }
+        };
+
+        fetchRandomMovie();
+    }, []);
+
     return (
         <div className="container mx-auto p-4 min-h-screen">
             <h1 className="text-4xl font-bold mb-6 text-center text-white">Movies</h1>
             <div className="flex justify-center mb-6">
                 <input
                     type="text"
-                    placeholder="Search for movies..."
+                    placeholder={randomMovieId}
                     value={searchTerm}
                     onChange={(e) => setSearchTerm(e.target.value)}
                     className="p-3 w-full max-w-md border border-gray-400 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
