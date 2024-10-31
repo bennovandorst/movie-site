e2a7f1c3908b8ebd510bad5c4a3a7350f1e825b5
bennovandorst <9024828@student.zadkine.nl>
2024-10-31 19:57:56 +0100

    0% PHP 100% Sigma

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = () => {
             <div className="container mx-auto px-4 py-2 flex justify-between items-center">
                 <div className="hidden md:flex space-x-6">
                     <a href="/movie-site" className="text-gray-300 hover:text-white">Home</a>
-                    <a href={`/watch/${randomMovieId}`} className="block text-gray-300 hover:text-white">Random Movie</a>
+                    <a href={`/movie-site/watch/${randomMovieId}`} className="block text-gray-300 hover:text-white">Random Movie</a> {/* wtf is this */}
                 </div>
             </div>
         </nav>
