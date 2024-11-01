82b61e85aa5d8a012f87c70af381560a7f0eb3e5
bennovandorst <9024828@student.zadkine.nl>
2024-11-01 17:17:36 +0100

    In mijn brainrot era

--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -7,7 +7,7 @@ const NoPage = () => {
                 <h1 className="text-6xl font-bold text-white">404</h1>
                 <p className="text-2xl text-gray-400 mt-4">Page Not Found</p>
                 <p className="text-gray-500 mt-2">The page you are looking for does not exist.</p>
-                <a href="/" className="mt-6 inline-block px-6 py-3 bg-blue-600 text-white font-semibold rounded-md hover:bg-blue-700">
+                <a href="/movie-site" className="mt-6 inline-block px-6 py-3 bg-blue-600 text-white font-semibold rounded-md hover:bg-blue-700">
                     Go to Home
                 </a>
             </div>
