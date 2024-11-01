58bcef9ca7aee7ccf4f88b944f3616236b2e1d8c
bennovandorst <9024828@student.zadkine.nl>
2024-11-01 17:09:43 +0100

    this might work

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import MovieDetails from "./pages/MovieDetails";
 import MovieEmbed from "./components/MovieEmbed";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import NoPage from "./pages/NoPage";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="*" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/movie-site/movie/:id" element={<MovieDetails />} />
         <Route path="/movie-site/watch/:id" element={<MovieEmbed />} />
+        <Route path="*" element={<NoPage />} />
       </Routes>
       <Footer />
     </Router>
---
+++ src/pages/NoPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const NoPage = () => {
+    return (
+        <div className="flex items-center justify-center h-screen bg-gray-900">
+            <div className="text-center">
+                <h1 className="text-6xl font-bold text-white">404</h1>
+                <p className="text-2xl text-gray-400 mt-4">Page Not Found</p>
+                <p className="text-gray-500 mt-2">The page you are looking for does not exist.</p>
+                <a href="/" className="mt-6 inline-block px-6 py-3 bg-blue-600 text-white font-semibold rounded-md hover:bg-blue-700">
+                    Go to Home
+                </a>
+            </div>
+        </div>
+    );
+};
+
+export default NoPage;
